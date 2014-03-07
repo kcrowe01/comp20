7b50c61537f779e4d044b7b399fd38ad6af1584c
kcrowe01 <Kenneth.Crowell@tufts.edu>
2014-03-06 21:17:58 -0500

    geolocation

--- transit/map.js
+++ transit/map.js
@@ -1,14 +1,14 @@
 function load()
 {
-	navigator.geolocation.getCurrentPosition(function(position)){
-		myLat = position.coords.latitude;
-		myLng = position.coords.longitude;
+	//navigator.geolocation.getCurrentPosition(function(position)){
+	//	myLat = position.coords.latitude;
+	//	myLng = position.coords.longitude;
 	var mapOptions = {
           center: new google.maps.LatLng(24, 82),
           zoom: 8
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
-    }
+    //}
 }
 google.maps.event.addDomListener(window, 'load', load);

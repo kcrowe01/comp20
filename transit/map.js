d3ae94b011b99329d242421849ad53b164107334
kcrowe01 <Kenneth.Crowell@tufts.edu>
2014-03-13 22:55:05 -0400

    added missing red line stations

--- transit/map.js
+++ transit/map.js
@@ -75,7 +75,7 @@ function placeStation(map)
     'Red,Shawmut,42.29312583,-71.06573796000001^' +
     'Red,Ashmont,42.284652,-71.06448899999999^' +
     'Red,North Quincy,42.275275,-71.029583^' +
-    'Red,Wollaston,42.2665139,-71.0203369^'
+    'Red,Wollaston,42.2665139,-71.0203369^' +
     'Red,Quincy Center,42.251809,-71.005409^' +
     'Red,Quincy Adams,42.233391,-71.007153^' +
     'Red,Braintree,42.2078543,-71.0011385^';
